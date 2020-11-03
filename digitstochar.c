388637695703a1b82e3be27d2a319519fae3c7b5
M SATHYA KUMAR <41425521+msathyakumar@users.noreply.github.com>
2020-11-03 13:08:54 +0530

    Create digitstochar.c

---
+++ digitstochar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a,b,c,d;
+    printf("enter the character based numbers");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    printf("%c-%d %c-%d %c-%d %c-%d",a,a,b,b,c,c,d,d);
+    
+    getch();
+    
+}
